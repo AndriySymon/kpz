f4ae7e4aba5b34996e2c81e55fc33ad9c7d078a4
AndriySymon <ipz235_sag@student.ztu.edu.ua>
2025-03-24 22:10:32 +0200

    Лабораторні

---
+++ lab-03/ClassLibrary_5/LightHTMLNodeFactory_task6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_5
+{
+    public class LightHTMLNode_task6
+    {
+        private static Dictionary<string, LightElementNode> elementCache = new Dictionary<string, LightElementNode>();
+
+        public static LightElementNode GetElementNode(string tagName, string displayType, bool isSelfClosing = false)
+        {
+            string key = $"{tagName}-{displayType}-{isSelfClosing}";
+            if (!elementCache.ContainsKey(key))
+            {
+                elementCache[key] = new LightElementNode(tagName, displayType, isSelfClosing);
+            }
+            return elementCache[key];
+        }
+
+        public static LightTextNode GetTextNode(string text)
+        {
+            return new LightTextNode(text);
+        }
+    }
+}
---
+++ lab-03/task6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary_5;
+
+namespace task6
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://www.gutenberg.org/cache/epub/1513/pg1513.txt";
+            string bookText = await DownloadBookText(url);
+
+            string[] lines = bookText.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+            List<LightNode> htmlNodes = new List<LightNode>();
+
+            foreach (var line in lines)
+            {
+                LightElementNode elementNode = null;
+
+                if (line == lines[0])
+                {
+                    elementNode = LightHTMLNode_task6.GetElementNode("h1", "block");
+                }
+                else if (line.Length < 20)
+                {
+                    elementNode = LightHTMLNode_task6.GetElementNode("h2", "block");
+                }
+                else if (line.StartsWith(" "))
+                {
+                    elementNode = LightHTMLNode_task6.GetElementNode("blockquote", "block");
+                }
+                else
+                {
+                    elementNode = LightHTMLNode_task6.GetElementNode("p", "block");
+                }
+
+                elementNode.AddChild(LightHTMLNode_task6.GetTextNode(line));
+                htmlNodes.Add(elementNode);
+            }
+
+            foreach (var node in htmlNodes)
+            {
+                Console.WriteLine(node.OuterHTML);
+            }
+
+            long memoryUsage = System.GC.GetTotalMemory(true);
+            Console.WriteLine($"Memory used by the HTML tree: {memoryUsage} bytes");
+        }
+
+        static async Task<string> DownloadBookText(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                return await client.GetStringAsync(url);
+            }
+        }
+    }
+}
